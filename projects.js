03eabea64ec8e0fcc07a5710a80ef2579aa6cf69
Ching-Chih Tsao <plack537@gmail.com>
2024-03-12 11:58:27 -0600

    projects and publication responsive init

---
+++ projects.js
@@ -0,0 +1,29 @@
+var mql = window.matchMedia("(min-width: 600px)");
+function responsive() {
+    responsiveLogo();
+    responsiveNavList();
+}
+
+function responsiveLogo() {
+    var logo = document.getElementById("logo");
+    var logoName = document.getElementById("logo-name");
+    // var sectionHome = document.getElementById("home");
+    logo.style.paddingTop = mql.matches ? "3vw" : "7vw";
+    logo.style.paddingLeft = mql.matches ? "6vw" : "11vw";
+    // sectionHome.style.paddingTop = mql.matches ? "150px" : "100px";
+    // logoName.innerHTML = mql.matches ? "Ching-Chih Amber Tsao" : "C. Amber Tsao";
+}
+
+function responsiveNavList() {
+    var homeNav = document.getElementById("home-nav");
+    var pubNav = document.getElementById("pub-nav");
+    var proNav = document.getElementById("pro-nav");
+    var blogNav = document.getElementById("blog-nav");
+    homeNav.innerHTML = mql.matches ? "Home" : "<i class='fi fi-sr-home'></i>";
+    pubNav.innerHTML = mql.matches ? "Publications" : "<i class='fi fi-sr-pen-nib'></i>";
+    proNav.innerHTML = mql.matches ? "Projects" : "<i class='fi fi-sr-hammer'></i>";
+    blogNav.innerHTML = mql.matches ? "Blog" : "<img src='icon/medium.png' width='18px'>";
+}
+
+mql.onchange = responsive;
+responsive();
