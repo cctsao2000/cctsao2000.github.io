7a55008adf656d7e7992cf3668ee5a8290e65242
Amber Tsao <cctsao2000@gmail.com>
2024-08-31 13:35:52 -0400

    close projects page

--- publications.js
+++ publications.js
@@ -68,7 +68,7 @@ function responsiveNavList() {
     var blogNav = document.getElementById("blog-nav");
     homeNav.innerHTML = mql.matches ? "Home" : "<i class='fi fi-sr-home'></i>";
     pubNav.innerHTML = mql.matches ? "Publications" : "<i class='fi fi-sr-pen-nib'></i>";
-    proNav.innerHTML = mql.matches ? "Projects" : "<i class='fi fi-sr-hammer'></i>";
+    // proNav.innerHTML = mql.matches ? "Projects" : "<i class='fi fi-sr-hammer'></i>";
     blogNav.innerHTML = mql.matches ? "Blog" : "<img src='icon/medium.png' width='18px'>";
 }
 
