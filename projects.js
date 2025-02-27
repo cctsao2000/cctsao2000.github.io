86824978073031664d44dede6cd006ae228a11d2
Amber Tsao <cctsao2000@gmail.com>
2025-02-26 22:20:26 -0500

    projects gallery done

--- projects.js
+++ projects.js
@@ -50,55 +50,45 @@ async function publishProjects() {
     projects.forEach(project => {
         const projectBlock = document.createElement("li");
         projectBlock.className = "project";
-        projectBlock.dataset.tags = project.tags ? project.tags.join(",") : "";
+        projectBlock.dataset.tags = project.tags ? project.tags.join(",") : ""; 
 
         // Cover Photo
         if (project.coverPhoto) {
             const coverPhoto = document.createElement("img");
             coverPhoto.src = project.coverPhoto;
             coverPhoto.alt = project.name;
+            coverPhoto.className = "project-cover";
             projectBlock.appendChild(coverPhoto);
         }
+
+        // Title Container
+        const titleContainer = document.createElement("div");
+        titleContainer.className = "project-title-container";
 
         // Title
         const title = document.createElement("h2");
         title.textContent = project.name;
-        projectBlock.appendChild(title);
+        title.className = "project-title";
+        titleContainer.appendChild(title);
 
-        // Year and Collaborators
-        const metaInfo = document.createElement("p");
-        metaInfo.textContent = `${project.projectYearMonth} | Collaborators: ${project.collaborator || "None"}`;
-        projectBlock.appendChild(metaInfo);
+        // Horizontal Line
+        const hr = document.createElement("hr");
+        hr.className = "project-separator";
+        titleContainer.appendChild(hr);
 
-        // Intro
-        const intro = document.createElement("p");
-        intro.textContent = project.intro;
-        projectBlock.appendChild(intro);
+        // Project Year Month
+        const yearMonth = document.createElement("p");
+        yearMonth.textContent = project.projectYearMonth;
+        yearMonth.className = "project-year-month";
+        titleContainer.appendChild(yearMonth);
 
-        // // Tags
-        // if (project.tags && project.tags.length > 0) {
-        //     const tagContainer = document.createElement("div");
-        //     tagContainer.className = "project-tags";
+        projectBlock.appendChild(titleContainer);
 
-        //     project.tags.forEach(tag => {
-        //         const tagElement = document.createElement("span");
-        //         tagElement.textContent = tag;
-        //         tagElement.className = "project-tag";
-        //         tagContainer.appendChild(tagElement);
-        //     });
+        projectsList.appendChild(projectBlock);
 
-        //     projectBlock.appendChild(tagContainer);
-        // }
-
-        // Link to Project Page
-        if (project.page) {
-            const projectLink = document.createElement("a");
-            projectLink.href = project.page;
-            projectLink.textContent = "View Details";
-            projectLink.className = "project-link";
-            projectBlock.appendChild(projectLink);
-        }
-
+        const overlay = document.createElement("div");
+        overlay.className = "project-overlay";
+        projectBlock.appendChild(overlay);
         projectsList.appendChild(projectBlock);
     });
 
@@ -119,95 +109,29 @@ async function publishProjects() {
     });
 }
 
+var mql = window.matchMedia("(min-width: 600px)");
+function responsive() {
+    responsiveLogo();
+    responsiveNavList();
+}
+
+function responsiveLogo() {
+    var logo = document.getElementById("logo");
+    logo.style.paddingTop = mql.matches ? "3vw" : "7vw";
+    logo.style.paddingLeft = mql.matches ? "6vw" : "11vw";
+}
+
+function responsiveNavList() {
+    var homeNav = document.getElementById("home-nav");
+    var pubNav = document.getElementById("pub-nav");
+    var proNav = document.getElementById("pro-nav");
+    var blogNav = document.getElementById("blog-nav");
+    homeNav.innerHTML = mql.matches ? "Home" : "<i class='fi fi-sr-home'></i>";
+    pubNav.innerHTML = mql.matches ? "Publications" : "<i class='fi fi-sr-pen-nib'></i>";
+    proNav.innerHTML = mql.matches ? "Projects" : "<i class='fi fi-sr-hammer'></i>";
+    blogNav.innerHTML = mql.matches ? "Blog" : "<img src='icon/medium.png' width='18px' style='margin-right:0px'>";
+}
+
+mql.onchange = responsive;
+responsive();
 publishProjects();
-
-// async function fetchProjects() {
-//     const projectsResponse = await fetch('data/projects.json');
-//     const projectsData = await projectsResponse.json();
-//     return projectsData;
-// }
-
-// async function publishProjects() {
-//     projects = await fetchProjects();
-//     projectsList = document.querySelector("#full-pro-list");
-//     projects.forEach((project) => {
-//         const projectBlock = document.createElement("li");
-//         projectBlock.className = "project";
-
-//         // Cover Photo
-//         if (project.coverPhoto) {
-//             const coverPhoto = document.createElement("img");
-//             coverPhoto.src = project.coverPhoto;
-//             coverPhoto.alt = project.name;
-//             projectBlock.appendChild(coverPhoto);
-//         }
-
-//         // Title
-//         const title = document.createElement("h2");
-//         title.textContent = project.name;
-//         projectBlock.appendChild(title);
-
-//         // Year and Collaborators
-//         const metaInfo = document.createElement("p");
-//         metaInfo.textContent = `${project.projectYearMonth} | Collaborators: ${project.collaborator || "None"}`;
-//         projectBlock.appendChild(metaInfo);
-
-//         // Intro
-//         const intro = document.createElement("p");
-//         intro.textContent = project.intro;
-//         projectBlock.appendChild(intro);
-
-//         // Tags
-//         if (project.tags && project.tags.length > 0) {
-//             const tagContainer = document.createElement("div");
-//             tagContainer.className = "project-tags";
-
-//             project.tags.forEach((tag) => {
-//                 const tagElement = document.createElement("span");
-//                 tagElement.textContent = tag;
-//                 tagElement.className = "project-tag";
-//                 tagContainer.appendChild(tagElement);
-//             });
-
-//             projectBlock.appendChild(tagContainer);
-//         }
-
-//         // Link to Project Page
-//         if (project.page) {
-//             const projectLink = document.createElement("a");
-//             projectLink.href = project.page;
-//             // projectLink.textContent = "View Details";
-//             projectLink.className = "project-link";
-//             projectBlock.appendChild(projectLink);
-//         }
-
-//         projectsList.appendChild(projectBlock);
-//     });
-// }
-
-// var mql = window.matchMedia("(min-width: 600px)");
-// function responsive() {
-//     responsiveLogo();
-//     responsiveNavList();
-// }
-
-// function responsiveLogo() {
-//     var logo = document.getElementById("logo");
-//     logo.style.paddingTop = mql.matches ? "3vw" : "7vw";
-//     logo.style.paddingLeft = mql.matches ? "6vw" : "11vw";
-// }
-
-// function responsiveNavList() {
-//     var homeNav = document.getElementById("home-nav");
-//     var pubNav = document.getElementById("pub-nav");
-//     var proNav = document.getElementById("pro-nav");
-//     var blogNav = document.getElementById("blog-nav");
-//     homeNav.innerHTML = mql.matches ? "Home" : "<i class='fi fi-sr-home'></i>";
-//     pubNav.innerHTML = mql.matches ? "Publications" : "<i class='fi fi-sr-pen-nib'></i>";
-//     proNav.innerHTML = mql.matches ? "Projects" : "<i class='fi fi-sr-hammer'></i>";
-//     blogNav.innerHTML = mql.matches ? "Blog" : "<img src='icon/medium.png' width='18px' style='margin-right:0px'>";
-// }
-
-// mql.onchange = responsive;
-// responsive();
-// publishProjects();
