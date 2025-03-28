b8eaba75cdbb0f7fedec9f0dcfa257784ed49e8d
Amber Tsao <cctsao2000@gmail.com>
2025-03-27 22:22:03 -0400

    add pub cover photo

--- publications.js
+++ publications.js
@@ -10,80 +10,106 @@ async function publishPublications() {
     publicationsList = document.querySelector("#full-pub-list");
     abstracts = await fetchPublications("abstract");
     abstractsList = document.querySelector("#full-abs-list");
+    
     for (publication in publications) {
         var publicationBlock = document.createElement("li");
+        publicationBlock.className = "pub-item";
+        
+        var coverImage = document.createElement("img");
+        coverImage.src = publications[publication].coverImage;
+        coverImage.alt = "Cover Image";
+        coverImage.className = "pub-cover";
+        publicationBlock.appendChild(coverImage);
+        
+        var contentBlock = document.createElement("div");
+        contentBlock.className = "pub-content";
+        
         var title = document.createElement("p");
         title.textContent = publications[publication].title;
         title.className = "pub-title";
-        publicationBlock.appendChild(title);
-
+        contentBlock.appendChild(title);
+        
         var author = document.createElement("p");
         author.textContent = publications[publication].authorship;
         author.className = "pub-author";
-        publicationBlock.appendChild(author);
-
+        contentBlock.appendChild(author);
+        
         var conference = document.createElement("p");
         conference.textContent = publications[publication].conference;
         conference.className = "pub-conf";
-        publicationBlock.appendChild(conference);
-
+        contentBlock.appendChild(conference);
+        
         var doiTag = document.createElement("a");
         doiTag.textContent = "DOI";
         doiTag.href = publications[publication].doi;
         doiTag.className = "pub-tag blue";
-        publicationBlock.appendChild(doiTag);
-
+        contentBlock.appendChild(doiTag);
+        
         var pdfTag = document.createElement("a");
         pdfTag.textContent = "PDF";
         pdfTag.href = publications[publication].pdf;
         pdfTag.className = "pub-tag red";
-        publicationBlock.appendChild(pdfTag);
-
+        contentBlock.appendChild(pdfTag);
+        
         var materialsList = publications[publication].otherMaterials;
         for (material in materialsList) {
             var materialTag = document.createElement("a");
             materialTag.textContent = materialsList[material].name;
             materialTag.href = materialsList[material].link;
             materialTag.className = "pub-tag";
-            publicationBlock.appendChild(materialTag);
+            contentBlock.appendChild(materialTag);
         }
-
+        
+        publicationBlock.appendChild(contentBlock);
         publicationsList.appendChild(publicationBlock);
     }
+    
     for (abstract in abstracts) {
         var abstractBlock = document.createElement("li");
+        abstractBlock.className = "pub-item";
+        
+        var coverImage = document.createElement("img");
+        coverImage.src = abstracts[abstract].coverImage;
+        coverImage.alt = "Cover Image";
+        coverImage.className = "pub-cover";
+        abstractBlock.appendChild(coverImage);
+        
+        var contentBlock = document.createElement("div");
+        contentBlock.className = "pub-content";
+        
         var title = document.createElement("p");
         title.textContent = abstracts[abstract].title;
         title.className = "pub-title";
-        abstractBlock.appendChild(title);
-
+        contentBlock.appendChild(title);
+        
         var author = document.createElement("p");
         author.textContent = abstracts[abstract].authorship;
         author.className = "pub-author";
-        abstractBlock.appendChild(author);
-
+        contentBlock.appendChild(author);
+        
         var conference = document.createElement("p");
         conference.textContent = abstracts[abstract].conference;
         conference.className = "pub-conf";
-        abstractBlock.appendChild(conference);
-
-        if (abstracts[abstract].pdf != null){
+        contentBlock.appendChild(conference);
+        
+        if (abstracts[abstract].pdf != null) {
             var pdfTag = document.createElement("a");
             pdfTag.textContent = "PDF";
             pdfTag.href = abstracts[abstract].pdf;
             pdfTag.className = "pub-tag red";
-            abstractBlock.appendChild(pdfTag);
+            contentBlock.appendChild(pdfTag);
         }
-
+        
         var materialsList = abstracts[abstract].otherMaterials;
         for (material in materialsList) {
             var materialTag = document.createElement("a");
             materialTag.textContent = materialsList[material].name;
             materialTag.href = materialsList[material].link;
             materialTag.className = "pub-tag";
-            abstractBlock.appendChild(materialTag);
+            contentBlock.appendChild(materialTag);
         }
-
+        
+        abstractBlock.appendChild(contentBlock);
         abstractsList.appendChild(abstractBlock);
     }
 }
